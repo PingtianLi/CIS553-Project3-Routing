5910c006827d456b5b101ff1f182f9d7e6ef88f5
James Xue <email@jxue.co>
2019-03-13 08:43:58 -0400

    started LS

--- LSrouter.py
+++ LSrouter.py
@@ -1,7 +1,7 @@
 ####################################################
 # LSrouter.py
-# Names:
-# Penn IDs:
+# Names: Tong Pow, James Xue
+# Penn IDs: 84233063, 51632014
 #####################################################
 
 import sys
@@ -17,6 +17,16 @@ class LSrouter(Router):
     def __init__(self, addr, heartbeatTime):
         """TODO: add your own class fields and initialization code here"""
         Router.__init__(self, addr)  # initialize superclass - don't remove
+        self.addr = addr
+        self.heartbeatTime = heartbeatTime
+        self.lastTime = 0
+        # self.neighbors = {} # directly connected neighbors: cost of link
+        self.tentative = {}
+        self.confirmed = {
+            'destination': addr,
+            'cost': 0,
+            'nextHop': None
+        }
 
 
     def handlePacket(self, port, packet):
@@ -34,9 +44,20 @@ class LSrouter(Router):
         pass
 
 
+    def sendRoutingPacket():
+        """Helper function that sends routing packets to all other nodes in network"""
+        # content = {
+        #     'neighbors': self.neighbors,
+        #     'sequence_num': 0
+        # }
+        p = Packet(kind, srcAddr=addr, dstAddr, content=dumps(content))
+
+
     def handleTime(self, timeMillisecs):
         """TODO: handle current time"""
-        pass
+        if timeMillisecs - self.lastTime > self.heartbeatTime:
+            self.sendRoutingPacket()
+            self.lastTime = timeMillisecs
 
 
     def debugString(self):
